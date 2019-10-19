7e8cab811297fbee4b6bf320494c3e5dd58e6f80
Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>
2019-10-19 23:27:58 +0300

    Added fields and properties
    Signed-off-by: Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>

--- SysPro_Lab_03/Computer.cs
+++ SysPro_Lab_03/Computer.cs
@@ -8,5 +8,38 @@ namespace SysPro_Lab_03
 {
     class Computer
     {
+        private static int currentID = 0;
+
+        public List<Device> devices { get; }
+
+        private Dictionary<PortType, int> totalPorts;
+
+        private Dictionary<PortType, int> availablePorts;
+
+        public int ID { get; set; }
+
+        public string[] Ports
+        {
+            get
+            {
+                string[] res = new string[totalPorts.Count];
+
+                var sb = new StringBuilder();
+
+                int i = 0;
+
+                foreach (var key in totalPorts.Keys)
+                {
+                    sb.Clear();
+                    sb.Append(key.ToString().Replace('_', ' '));
+                    sb.AppendFormat(":\t{0}/{1}", availablePorts[key], totalPorts[key]);
+
+                    res[i] = sb.ToString();
+                    ++i;
+                }
+
+                return res;
+            }
+        }
     }
 }
