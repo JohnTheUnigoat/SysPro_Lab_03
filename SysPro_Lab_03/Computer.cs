99c50e5899d6588d9e8f56dfa4a13b6c56fee65a
Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>
2019-10-19 15:06:03 +0300

    Created Computer class
    Signed-off-by: Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>

---
+++ SysPro_Lab_03/Computer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysPro_Lab_03
+{
+    class Computer
+    {
+    }
+}
