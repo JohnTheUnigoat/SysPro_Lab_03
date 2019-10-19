637bdd054c74dfe63f2b4b114e5139cc70b14beb
Olexandr Korotetskyi <ayrum1158@gmail.com>
2019-10-19 16:01:42 +0300

    Added data fields to Device class
    Signed-off-by: Olexandr Korotetskyi <ayrum1158@gmail.com>

--- SysPro_Lab_03/Device.cs
+++ SysPro_Lab_03/Device.cs
@@ -8,5 +8,24 @@ namespace SysPro_Lab_03
 {
     class Device
     {
+        public enum DeviceType { Mouse, Keyboard, HDD, Ram, Flash, Printer, Scaner, Speakers, Phone, Micrphone, Headphones, Monitor };
+
+        private DeviceType deviceType;
+
+        public string Name { get; private set; }
+
+        public string Manufacturer { get; private set; }
+
+        public string Type
+        {
+            get
+            {
+                return deviceType.ToString();
+            }
+        }
+
+        public PortType PortType { get; }
+
+        public bool IsConnected { get; set; }
     }
 }
