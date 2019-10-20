21bbfe8c503392747906b361d5c2b0f9179fac5d
Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>
2019-10-20 03:02:41 +0300

    Added fields and properties
    Signed-off-by: Ivan Hudzinskyi <ivanhudzinskyi2000@gmail.com>

--- SysPro_Lab_03/DeviceManager.cs
+++ SysPro_Lab_03/DeviceManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,69 @@ namespace SysPro_Lab_03
 {
     class DeviceManager
     {
+        //fields
+        private List<Device> devices;
+
+        private List<Computer> computers;
+
+        private SortedList<Device.DeviceType, int> totalDeviceCount;
+
+        private SortedList<Device.DeviceType, int> availableDeviceCount;
+
+        //properties
+        public ReadOnlyCollection<Computer> Computers
+        {
+            get
+            {
+                return computers.AsReadOnly();
+            }
+        }
+
+        public ReadOnlyCollection<Device> UnusedDevices
+        {
+            get
+            {
+                List<Device> res = new List<Device>();
+
+                foreach (var item in devices)
+                {
+                    if (!item.IsConnected)
+                        res.Add(item);
+                }
+
+                return res.AsReadOnly();
+            }
+        }
+
+        public string[] DeviceCount
+        {
+            get
+            {
+                string[] res = new string[totalDeviceCount.Count];
+
+                var sb = new StringBuilder();
+
+                int i = 0;
+
+                foreach (var key in totalDeviceCount.Keys)
+                {
+                    sb.Clear();
+                    sb.Append(key.ToString());
+                    sb.AppendFormat(":\t{0}/{1}", availableDeviceCount[key], totalDeviceCount[key]);
+
+                    res[i++] = sb.ToString();
+                }
+
+                return res;
+            }
+        }
+
+        public int ComputerCount
+        {
+            get
+            {
+                return Computers.Count;
+            }
+        }
     }
 }
